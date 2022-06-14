331dfe021031c9b0379ce79975a875019d072cc5
João Victor Amaral <joao.amaral@squadra.com.br>
2022-06-14 14:18:08 -0300

    Criei a interface ICreateUserUseCase e sua implementação

---
+++ GerenciadorDeCursos/GerenciadorDeCursos.Border/UseCases/ICreateUserUseCase.cs
@@ -0,0 +1,10 @@
+﻿using GerenciadorDeCursos.Border.DTOs.In;
+using GerenciadorDeCursos.Shared.Models;
+
+namespace GerenciadorDeCursos.Border.UseCases
+{
+    public interface ICreateUserUseCase
+    {
+        ResultBase CreateUser(CreateUserRequest request);
+    }
+}
---
+++ GerenciadorDeCursos/GerenciadorDeCursos.UseCases/UserUseCase/CreateUserUseCase.cs
@@ -0,0 +1,20 @@
+﻿using GerenciadorDeCursos.Border.DTOs.In;
+using GerenciadorDeCursos.Border.DTOs.Out;
+using GerenciadorDeCursos.Border.Entities;
+using GerenciadorDeCursos.Border.UseCases;
+using GerenciadorDeCursos.Shared.Models;
+
+namespace GerenciadorDeCursos.UseCases.UserUseCase
+{
+    public class CreateUserUseCase : ICreateUserUseCase {
+        public ResultBase CreateUser(CreateUserRequest request) {
+            ResultBase result = new ResultBase();
+            CreateUserResponse userResponse = new CreateUserResponse();
+            User createdUser = new User(request.Name,request.Password,request.Role);
+            userResponse.Name = createdUser.Name;
+            userResponse.Role = createdUser.Role;
+            result.Data = createdUser;
+            return result;
+        }
+    }
+}
