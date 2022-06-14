ef676f79c0377fd1165be3beefd03fbd6d0589f8
João Victor Amaral <joao.amaral@squadra.com.br>
2022-06-14 14:13:45 -0300

    Criei os Dtos de Request e Response

---
+++ GerenciadorDeCursos/GerenciadorDeCursos.Border/DTOs/In/CreateUserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace GerenciadorDeCursos.Border.DTOs.In
+{
+    public class CreateUserRequest
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ GerenciadorDeCursos/GerenciadorDeCursos.Border/DTOs/Out/CreateUserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace GerenciadorDeCursos.Border.DTOs.Out
+{
+    public class CreateUserResponse
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
