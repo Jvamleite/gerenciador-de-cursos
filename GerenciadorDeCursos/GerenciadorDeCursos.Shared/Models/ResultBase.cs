f372797d81637cd2fcd8b1e71ae7a0f026127584
João Victor Amaral <joao.amaral@squadra.com.br>
2022-06-14 14:15:17 -0300

    Criei o ResultBase

---
+++ GerenciadorDeCursos/GerenciadorDeCursos.Shared/Models/ResultBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GerenciadorDeCursos.Shared.Models
+{
+    public class ResultBase
+    {
+        public string Message { get; set; }
+        public bool sucess { get; set; }
+        public Object Data { get ; set; }
+
+        public ResultBase()
+        {
+                
+        }
+        public ResultBase(object data)
+        {
+            Data = data;
+        }
+    }
+
+    
+}
