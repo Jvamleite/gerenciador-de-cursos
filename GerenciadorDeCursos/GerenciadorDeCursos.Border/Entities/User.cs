650115e4ce74a3cca02ff0b7e869c063fb4cc213
João Victor Amaral <joao.amaral@squadra.com.br>
2022-06-14 14:13:05 -0300

    Criei a entidade User

---
+++ GerenciadorDeCursos/GerenciadorDeCursos.Border/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿namespace GerenciadorDeCursos.Border.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+        public User(string name, string password, string role)
+        {
+            Name = name;
+            Password = password;
+            Role = role;
+        }
+    }
+
+
+}
