84f65f73612eae27986121043660ab0e412dae69
João Victor Amaral <joao.amaral@squadra.com.br>
2022-06-15 15:11:45 -0300

    Fiz a injeção do IUserRepository no CreateUserUseCase e adicionei a injenção na classe Startup

--- GerenciadorDeCursos/GerenciadorDeCursos.API/Startup.cs
+++ GerenciadorDeCursos/GerenciadorDeCursos.API/Startup.cs
@@ -1,16 +1,11 @@
+using GerenciadorDeCursos.Border.Repositories;
+using GerenciadorDeCursos.Repositories.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GerenciadorDeCursos.API
 {
@@ -32,6 +27,8 @@ namespace GerenciadorDeCursos.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GerenciadorDeCursos.API", Version = "v1" });
             });
+
+            services.AddScoped<IUserRepository, UserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- GerenciadorDeCursos/GerenciadorDeCursos.UseCases/UserUseCase/CreateUserUseCase.cs
+++ GerenciadorDeCursos/GerenciadorDeCursos.UseCases/UserUseCase/CreateUserUseCase.cs
@@ -1,16 +1,26 @@
 ﻿using GerenciadorDeCursos.Border.DTOs.In;
 using GerenciadorDeCursos.Border.DTOs.Out;
 using GerenciadorDeCursos.Border.Entities;
+using GerenciadorDeCursos.Border.Repositories;
 using GerenciadorDeCursos.Border.UseCases;
 using GerenciadorDeCursos.Shared.Models;
 
 namespace GerenciadorDeCursos.UseCases.UserUseCase
 {
     public class CreateUserUseCase : ICreateUserUseCase {
+
+        private readonly IUserRepository _userRepositoy;
+
+        public CreateUserUseCase(IUserRepository userRepositoy)
+        {
+            _userRepositoy = userRepositoy; 
+        }
+
         public ResultBase CreateUser(CreateUserRequest request) {
             ResultBase result = new ResultBase();
             CreateUserResponse userResponse = new CreateUserResponse();
             User createdUser = new User(request.Name,request.Password,request.Role);
+            _userRepositoy.Add(createdUser);
             userResponse.Name = createdUser.Name;
             userResponse.Role = createdUser.Role;
             result.Data = createdUser;
