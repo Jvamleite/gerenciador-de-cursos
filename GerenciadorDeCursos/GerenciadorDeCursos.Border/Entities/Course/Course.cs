b6688556b32754a50a0cf62c38d6f6311dbd5d8e
João Victor Amaral <joao.amaral@squadra.com.br>
2022-06-21 23:33:45 -0300

    Criei a Entidade Course e um enum Status para servir de propriedade para Course

---
+++ GerenciadorDeCursos/GerenciadorDeCursos.Border/Entities/Course/Course.cs
@@ -0,0 +1,18 @@
+﻿using GerenciadorDeCursos.Border.Entities.Course.Enums;
+using System;
+
+namespace GerenciadorDeCursos.Border.Entities.Course
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Título { get; set; }
+
+        public DateTime DataInicial { get; set; }
+
+        public DateTime DataFinal { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
---
+++ GerenciadorDeCursos/GerenciadorDeCursos.Border/Entities/Course/Enums/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeCursos.Border.Entities.Course.Enums
+{
+    public enum Status
+    {
+        Previsto = 0,
+        EmAndamento = 1,
+        Concluido = 2,
+    }
+}
