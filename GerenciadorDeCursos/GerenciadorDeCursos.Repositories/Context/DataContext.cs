fadb261ed95e6f1dc534082848bf8b0b0d730fd0
João Victor Amaral <joao.amaral@squadra.com.br>
2022-06-23 16:22:55 -0300

    Fiz o mapeamento no banco da entidade Course

--- GerenciadorDeCursos/GerenciadorDeCursos.Repositories/Context/DataContext.cs
+++ GerenciadorDeCursos/GerenciadorDeCursos.Repositories/Context/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using GerenciadorDeCursos.Border.Entities.User;
+﻿using GerenciadorDeCursos.Border.Entities.Course;
+using GerenciadorDeCursos.Border.Entities.User;
 using Microsoft.EntityFrameworkCore;
 
 namespace GerenciadorDeCursos.Repositories.Data
@@ -6,6 +7,7 @@ namespace GerenciadorDeCursos.Repositories.Data
     public class DataContext :  DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Course> Courses { get; set; }
 
         public DataContext (DbContextOptions<DataContext> options) : base(options)
         {
---
+++ GerenciadorDeCursos/GerenciadorDeCursos.Repositories/Context/Mapping/CourseMap.cs
@@ -0,0 +1,23 @@
+﻿using GerenciadorDeCursos.Border.Entities.Course;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeCursos.Repositories.Context.Mapping
+{
+    public class CourseMap : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Título).HasMaxLength(20).IsRequired();
+            builder.Property(p => p.DataInicial).IsRequired();
+            builder.Property(p => p.DataFinal).IsRequired();
+            builder.Property(p => p.Status).HasConversion<string>().IsRequired();
+        }
+    }
+}
