a6bb3c4b5e28b84e0866592e72a6db1584578673
João Victor Amaral <joao.amaral@squadra.com.br>
2022-06-24 14:46:04 -0300

    Criei o repositorio do curso e implementei o método AddCourse

---
+++ GerenciadorDeCursos/GerenciadorDeCursos.Repositories/Repositories/CourseRepository.cs
@@ -0,0 +1,28 @@
+﻿using GerenciadorDeCursos.Border.Entities.Course;
+using GerenciadorDeCursos.Border.Repositories;
+using GerenciadorDeCursos.Repositories.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeCursos.Repositories.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly DataContext _context;
+
+        public CourseRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Course> AddCourseAsync(Course course)
+        {
+            await _context.Courses.AddAsync(course);
+            await _context.SaveChangesAsync();
+            return course;
+        }
+    }
+}
