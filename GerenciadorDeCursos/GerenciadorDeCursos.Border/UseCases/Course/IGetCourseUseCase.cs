c15fbf289b297ac64f196941a577e425020e288b
João Victor Amaral de Magalhães <joao.amaral@squadra.com.br>
2022-06-27 19:48:31 -0300

    Fiz as interface IGetCourseUseCase e sua implementação

---
+++ GerenciadorDeCursos/GerenciadorDeCursos.Border/UseCases/Course/IGetCourseUseCase.cs
@@ -0,0 +1,10 @@
+﻿using GerenciadorDeCursos.Shared.Models;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeCursos.Border.UseCases.Course
+{
+    public interface IGetCourseUseCase
+    {
+        Task<ResultBase> GetAll();
+    }
+}
--- GerenciadorDeCursos/GerenciadorDeCursos.UseCases/Course/GetCourseUseCase.cs
+++ GerenciadorDeCursos/GerenciadorDeCursos.UseCases/Course/GetCourseUseCase.cs
@@ -1,6 +1,40 @@
-﻿namespace GerenciadorDeCursos.UseCases.CourseUseCases
+﻿using GerenciadorDeCursos.Border.DTOs.Course.Response;
+using GerenciadorDeCursos.Border.Entities.Course;
+using GerenciadorDeCursos.Border.Repositories;
+using GerenciadorDeCursos.Border.UseCases.Course;
+using GerenciadorDeCursos.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeCursos.UseCases.CourseUseCases
 {
-    public class GetCourseUseCase
+    public class GetCourseUseCase : IGetCourseUseCase
     {
+        private readonly ICourseRepository _courseRepository;
+
+        public GetCourseUseCase(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public async Task<ResultBase> GetAll()
+        {
+            try
+            {
+                IEnumerable<Course> courses = await _courseRepository.GetAllAsync();
+                List<CourseResponse> courseResponses = new List<CourseResponse>();
+                foreach(Course course in courses)
+                {
+                    var courseResponse = course.CreateCourseResponse();
+                    courseResponses.Add(courseResponse);
+                }
+                return new ResultBase(courseResponses);
+            }
+            catch(Exception ex)
+            {
+                return new ResultBase(false, ex.Message);
+            }
+        }
     }
 }
