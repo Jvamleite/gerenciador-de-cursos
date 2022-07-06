336feddeae29ddd81fff580d12db4af55081f83d
João Victor Amaral de Magalhães <joao.amaral@squadra.com.br>
2022-07-06 14:31:07 -0300

    Esbocei a RequestController

---
+++ GerenciadorDeCursos/GerenciadorDeCursos.API/Controllers/RequestController.cs
@@ -0,0 +1,24 @@
+﻿using GerenciadorDeCursos.Border.DTOs.CreateUserRequest;
+using GerenciadorDeCursos.Border.UseCases.User;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GerenciadorDeCursos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+        private readonly ICreateUserUseCase _createUserUseCase;
+
+        [HttpPost]
+        public async Task<IActionResult> CreateARequest([FromBody] CreateUserRequest createUserRequest)
+        {
+            var result = _createUserUseCase.CreateUserAsync(createUserRequest);
+            return Ok(result);
+        }
+
+    }
+}
