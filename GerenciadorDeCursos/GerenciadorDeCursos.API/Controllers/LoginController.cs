fca986957154a214d04423ba23462247d4e092cb
João Victor Amaral de Magalhães <joao.amaral@squadra.com.br>
2022-07-14 22:43:39 -0300

    Criei a LoginController e o LoginDto

---
+++ GerenciadorDeCursos/GerenciadorDeCursos.API/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using GerenciadorDeCursos.Border.DTOs.User.Request;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeCursos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+      
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+        {
+        }
+
+    }
+}
---
+++ GerenciadorDeCursos/GerenciadorDeCursos.Border/DTOs/User/Request/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace GerenciadorDeCursos.Border.DTOs.User.Request
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
