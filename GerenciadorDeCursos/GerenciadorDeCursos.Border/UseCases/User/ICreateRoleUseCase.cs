70bd32d80a10d1fb603b344bb239c9e3cbe82f66
João Victor Amaral de Magalhães <joao.amaral@squadra.com.br>
2022-07-18 14:27:50 -0300

    Criei a UseCase para criação da Role

---
+++ GerenciadorDeCursos/GerenciadorDeCursos.Border/UseCases/User/ICreateRoleUseCase.cs
@@ -0,0 +1,12 @@
+﻿using GerenciadorDeCursos.Border.DTOs.User.Request;
+using GerenciadorDeCursos.Border.Repositories;
+using GerenciadorDeCursos.Shared.Models;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeCursos.Border.UseCases.User
+{
+    public interface ICreateRoleUseCase
+    {
+        Task<ResultBase> CreateRole(CreateRoleRequest createRoleRequest);
+    }
+}
---
+++ GerenciadorDeCursos/GerenciadorDeCursos.UseCases/User/CreateRoleUseCase.cs
@@ -0,0 +1,37 @@
+﻿using GerenciadorDeCursos.Border.DTOs.User.Request;
+using GerenciadorDeCursos.Border.Entities.UserEntities;
+using GerenciadorDeCursos.Border.Repositories;
+using GerenciadorDeCursos.Border.UseCases.User;
+using GerenciadorDeCursos.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeCursos.UseCases.User
+{
+    internal class CreateRoleUseCase : ICreateRoleUseCase
+    {
+        private readonly IRoleRepository _roleRepository;
+
+        public CreateRoleUseCase(IRoleRepository roleRepository)
+        {
+            _roleRepository = roleRepository;
+        }
+
+        public async Task<ResultBase> CreateRole(CreateRoleRequest createRoleRequest)
+        {
+            try
+            {
+                var role = new Role(createRoleRequest.RoleName);
+                await _roleRepository.AddRoleAsync(role);
+                return new ResultBase(role);
+            }
+            catch(Exception ex)
+            {
+                return new ResultBase(false, ex.Message);
+            }
+        }
+    }
+}
