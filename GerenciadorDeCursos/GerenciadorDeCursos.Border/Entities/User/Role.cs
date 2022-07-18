92ac4fdf26173a9293258dd4da65523f0fc0ba24
João Victor Amaral de Magalhães <joao.amaral@squadra.com.br>
2022-07-18 12:47:42 -0300

    Mudando o conceito de role para classe pra viabilizar o tokenService

---
+++ GerenciadorDeCursos/GerenciadorDeCursos.Border/Entities/User/Role.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GerenciadorDeCursos.Border.Entities.User
+{
+    public class Role
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
